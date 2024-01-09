4f7276d58fd8466ebc59b9fd3865e7be279a32cf
neuman <owiroadams@gmail.com>
2024-01-09 13:05:05 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('form').addEventListener('submit',(e) => {
+    e.preventDefault();
+    buildToDo(e.target.querySelector('#new-task-description').value);
+  })
 });
+
+function buildToDo(task) {
+  let p = document.createElement('p');
+  p.textContent = `${task} `;
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  btn.addEventListener('click',handleDelete)
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
